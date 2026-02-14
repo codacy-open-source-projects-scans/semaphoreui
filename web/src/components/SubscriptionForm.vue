c621d3b9ea174fdccc2ef130e78956b622761113
Codacy Archive <codacy-archive@local>
2026-02-14 01:18:37 +0000

    snapshot 2026-02-14T01:18:37.816677+00:00

--- web/src/components/SubscriptionForm.vue
+++ web/src/components/SubscriptionForm.vue
@@ -35,13 +35,13 @@
       </div>
     </div>
 
-    <div v-else style="margin-bottom: 30px">
+    <div v-else style="margin-bottom: 30px; position: relative">
       <div
         v-if="item.state === 'active'"
         style="line-height: 1.3; font-weight: bold; color: rgb(0, 188, 0)"
         class="mb-5"
       >
-        You PRO subscription is active.
+        You {{ item.plan.startsWith('enterprise_') ? 'Enterprise' : 'PRO' }} subscription is active.
       </div>
       <div v-else style="line-height: 1.3">
         Enter your subscription key to unlock advanced features, or get a new one instantly.
@@ -52,13 +52,43 @@
         rows="4"
         auto-grow
         v-model="item.key"
-        label="Enter your PRO key"
+        label="Enter your PRO or EE key"
         :rules="[(v) => !!v || $t('key_required')]"
         required
         :disabled="formSaving || item.managed_by_config"
         outlined
         dense
       ></v-textarea>
+
+      <v-menu offset-y v-if="item.state === 'active'">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="primary"
+            v-bind="attrs"
+            v-on="on"
+            fab
+            small
+            style="position: absolute; top: 15px; right: -15px"
+          >
+            <v-icon>mdi-dots-horizontal</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item link @click="reloadToken">
+            <v-list-item-icon>
+              <v-icon>mdi-refresh</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Reload</v-list-item-title>
+          </v-list-item>
+          <v-list-item link @click="resetToken">
+            <v-list-item-icon>
+              <v-icon>mdi-delete</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Reset</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
       <v-row>
         <v-col>
@@ -74,7 +104,7 @@
               color="white"
               :size="24"
             ></v-progress-circular>
-            <span v-else>Activate New key</span>
+            <span v-else>Activate New Key</span>
           </v-btn>
         </v-col>
         <v-col>
@@ -106,6 +136,15 @@
     <v-card v-if="item.plan" class="mb-3" style="background: var(--highlighted-card-bg-color)">
       <v-card-title>Plan &amp; status</v-card-title>
       <v-card-text class="pb-2">
+        <v-list class="py-0 pb-5" style="background: unset" v-if="item.company">
+          <v-list-item class="pa-0">
+            <v-list-item-content class="py-0">
+              <v-list-item-title>Subscription holder</v-list-item-title>
+              <v-list-item-subtitle>{{ item.company }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
         <v-row>
           <v-col class="py-0">
             <v-list class="py-0" style="background: unset">
@@ -125,11 +164,11 @@
                 <v-list-item-content>
                   <v-list-item-title>Nodes</v-list-item-title>
                   <v-list-item-subtitle>
-                    {{ item.nodes }}
+                    {{ item.nodes_used }} / {{ item.nodes }}
                   </v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
-              <v-list-item class="pa-0">
+              <v-list-item class="pa-0" v-if="item.runners < 100000">
                 <v-list-item-content>
                   <v-list-item-title>Project runners</v-list-item-title>
                   <v-list-item-subtitle>
@@ -159,17 +198,25 @@
                   </v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
-              <v-list-item class="pa-0">
+              <v-list-item class="pa-0" v-if="item.users < 100000">
                 <v-list-item-content>
                   <v-list-item-title>Pro users</v-list-item-title>
                   <v-list-item-subtitle>{{ item.used }} / {{ item.users }}</v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
-              <v-list-item class="pa-0">
+              <v-list-item class="pa-0" v-if="item.terraform_states < 100000">
                 <v-list-item-content>
                   <v-list-item-title>Terraform backends</v-list-item-title>
                   <v-list-item-subtitle>
                     {{ item.terraform_states_used }} / {{ item.terraform_states }}
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item class="pa-0" v-if="item.uis">
+                <v-list-item-content>
+                  <v-list-item-title>UIs</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{ item.uis_used }} / {{ item.uis }}
                   </v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
@@ -179,6 +226,7 @@
 
         <div style="margin-top: 20px; font-weight: bold; color: #00bc00">
           Renews in {{ (new Date() - new Date(item.expiresAt)) | formatMilliseconds }}
+          <span>(if auto-renew is activated)</span>
         </div>
       </v-card-text>
     </v-card>
@@ -237,6 +285,30 @@ export default {
   },
 
   methods: {
+    async resetToken() {
+      this.formError = null;
+      this.formSaving = true;
+      try {
+        await axios.delete('/api/subscription');
+        await this.loadData();
+      } catch (err) {
+        this.formError = getErrorMessage(err);
+      } finally {
+        this.formSaving = false;
+      }
+    },
+    async reloadToken() {
+      this.formError = null;
+      this.formSaving = true;
+      try {
+        await axios.post('/api/subscription/refresh');
+        await this.loadData();
+      } catch (err) {
+        this.formError = getErrorMessage(err);
+      } finally {
+        this.formSaving = false;
+      }
+    },
     afterLoadData() {
       if (this.item.error) {
         this.formError = this.item.error;
